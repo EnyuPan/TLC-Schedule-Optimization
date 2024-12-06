ffa4d3db17fea5f07ddef55b4ffaf7b55311a436
Enyu Pan <101580450@georgebrown.ca>
2024-12-06 10:14:34 -0500

    Add lower-availability version of availability spreadsheet

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ try:
         demand_cells = pd.read_csv("demand_matrix_3d.csv", header=None)
         availability_cells = pd.read_csv("availability_matrix_4d.csv", header=None)
     else:
-        demand_cells = pd.read_csv("Lite version/demand_matrix_2d.csv", header=None)
+        demand_cells = pd.read_csv("Lite version/availability_matrix_3d_loweravailability.csv", header=None)
         availability_cells = pd.read_csv("Lite version/availability_matrix_3d.csv", header=None)
     desired_hours_cells = pd.read_csv("desired_hours.csv", header=None)
 except Exception as error:
